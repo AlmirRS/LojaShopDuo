802826e8bedc74908c36d31b6cb5f009e6dc8430
Almir Ramos <93881716+AlmirRS@users.noreply.github.com>
2023-01-17 07:17:52 -0300

    my first commit

---
+++ app/src/main/java/com/tech/ng/shopduo/form_cadastro/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package com.tech.ng.shopduo.form_cadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tech.ng.shopduo.R
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
---
+++ app/src/main/java/com/tech/ng/shopduo/form_login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tech.ng.shopduo.form_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tech.ng.shopduo.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
